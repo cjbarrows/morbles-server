98d6af09c6b2f58ff4727c7b2fc2e0152fe20df6
Charlie Barrows <charlie.barrows@gmail.com>
2022-02-15 17:12:45 -0500

    added login user to session

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	userkey = "user"
 )
 
 // album represents data about a record album.
@@ -74,8 +80,7 @@ func getAlbums(c *gin.Context) {
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
+	// Call BindJSON to bind the received JSON to newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
@@ -171,20 +176,95 @@ func putPlayerById(c *gin.Context) {
 	c.JSON(http.StatusNotFound, gin.H{"message": "player not found"})
 }
 
+func login(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+
+	for _, pl := range players {
+		if username == pl.Name && password == "2020" {
+			session.Set(userkey, username) // In real world usage you'd set this to the users ID
+			if err := session.Save(); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+}
+
+func getUser(c *gin.Context) (string, bool) {
+	session := sessions.Default(c)
+	user := session.Get(userkey)
+	return user.(string), true
+}
+
+func logout(c *gin.Context) {
+	if _, ok := getUser(c); !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+		return
+	}
+	session := sessions.Default(c)
+	session.Delete(userkey)
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+}
+
+func me(c *gin.Context) {
+	user, _ := getUser(c)
+	c.JSON(http.StatusOK, gin.H{"user": user})
+}
+
+func status(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "You are logged in"})
+}
+
+func AuthRequired(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userkey)
+	if user == nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+	c.Next()
+}
+
 func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
+	router.Use(sessions.Sessions("mysession", sessions.NewCookieStore([]byte("secret"))))
 
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.POST("/login", login)
+	router.GET("/logout", logout)
 
-	router.GET("/levels", getLevels)
-	router.GET("/levels/ids", getLevelsIDs)
-	router.GET("/levels/:id", getLevelByID)
+	private := router.Group("/api")
+	private.Use(AuthRequired)
+	{
+		private.GET("/me", me)
+		private.GET("/status", status)
 
-	router.GET("/player/:id", getPlayerByID)
-	router.PUT("/player/:id", putPlayerById)
+		private.GET("/albums", getAlbums)
+		private.GET("/albums/:id", getAlbumByID)
+		private.POST("/albums", postAlbums)
+
+		private.GET("/levels", getLevels)
+		private.GET("/levels/ids", getLevelsIDs)
+		private.GET("/levels/:id", getLevelByID)
+
+		private.GET("/player/:id", getPlayerByID)
+		private.PUT("/player/:id", putPlayerById)
+	}
 
 	router.Run("localhost:8080")
 }
