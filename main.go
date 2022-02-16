7c80ae514ae40d2b9ab4cda1bf8aa48f427ab24b
Charlie Barrows <charlie.barrows@gmail.com>
2022-02-16 10:56:20 -0500

    remove sample code

--- main.go
+++ main.go
@@ -15,14 +15,6 @@ const (
 	userkey = "user"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 type level struct {
 	ID            uint16
 	Name          string
@@ -46,13 +38,6 @@ type player struct {
 	LevelStatuses []levelStatus
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 var levels = []level{
 	{ID: 1, Name: "Nothing Doin'", Hint: "Ball drop.", Rows: 2, Columns: 1, StartingBalls: "R", EndingBalls: "R", MapData: " " + " "},
 	{ID: 2, Name: "Two Wide", Hint: "Pick a chute.", Rows: 2, Columns: 2, StartingBalls: "RG", EndingBalls: "RG", MapData: "  " + "  "},
@@ -69,41 +54,6 @@ var levelStatuses = []levelStatus{
 
 var players = []player{
 	{ID: 1, Name: "Test Player", LevelStatuses: levelStatuses},
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop through the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func getLevelIDs(theLevels []level) []uint16 {
@@ -253,10 +203,6 @@ func main() {
 	{
 		private.GET("/me", me)
 		private.GET("/status", status)
-
-		private.GET("/albums", getAlbums)
-		private.GET("/albums/:id", getAlbumByID)
-		private.POST("/albums", postAlbums)
 
 		private.GET("/levels", getLevels)
 		private.GET("/levels/ids", getLevelsIDs)
