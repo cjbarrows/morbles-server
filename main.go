d52fceb1ff568e741cb6b141f7ec0fbbd82fb735
Charlie Barrows <charlie.barrows@gmail.com>
2022-02-24 16:02:14 -0500

    trying https for CORS origin

--- main.go
+++ main.go
@@ -310,7 +310,7 @@ func main() {
 	router := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:4200", "http://morbles.herokuapp.com"}
+	config.AllowOrigins = []string{"http://localhost:4200", "https://morbles.herokuapp.com"}
 	config.AllowCredentials = true
 	config.AllowMethods = []string{"GET", "POST", "PUT"}
 	router.Use(cors.New(config))
